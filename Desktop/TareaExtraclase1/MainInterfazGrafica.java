f50202c9331a4f52e6d21c3b603cba16995b1132
github_Chris-0307 <chrisnavae@gmail.com>
2023-08-23 21:56:57 -0600

    Interfaz grafica finalizada

--- Desktop/TareaExtraclase1/MainInterfazGrafica.java
+++ Desktop/TareaExtraclase1/MainInterfazGrafica.java
@@ -1,26 +1,46 @@
-import java.awt.BorderLayout;
-
 import javax.swing.*;
 
 public class MainInterfazGrafica extends JFrame {
     MainInterfazGrafica(){
         
+        /*Declaraciones de variables en las interfaces graficas:
+         * 1. Caja de texto con lo que quieres escribir en el chat.
+         * 2. El chat
+         * 3. Botón para enviar el mensaje.
+         */
+
         JTextField texto_enviar = new JTextField();
         JTextArea texto_chat = new JTextArea();
         JButton boton_enviar = new JButton("Enviar");
-        
-        
 
-        JPanel panel_principal = new JPanel();
-        panel_principal.setLayout(new BorderLayout());
-        panel_principal.add(texto_chat, BorderLayout.NORTH);
-        panel_principal.add(texto_enviar, BorderLayout.CENTER);
-        panel_principal.add(boton_enviar, BorderLayout.SOUTH);
+        /*Tamaño de:
+         * 1. Botón
+         * 2. Caja de texto para escribir
+         */
 
-        add(panel_principal);
+        boton_enviar.setBounds(150, 575, 70, 35);
+        texto_enviar.setBounds(20, 525, 345, 30);
 
+        /*Barra deslizadora para ver los mensajes del chat.
+         * Tamaño del chat + barra deslizadora
+         */
+
+        JScrollPane barra = new JScrollPane(texto_chat, JScrollPane.VERTICAL_SCROLLBAR_ALWAYS, JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
+        barra.setBounds(20, 20, 345, 500);
+
+        /*Añadir elementos gráficos de todo el chat.
+         * Prohibir edición manual de texto al chat.
+         * Configuración de: estilo, título, tamaño, apagado, visibilidad.
+         */
+
+        add(boton_enviar);
+        add(texto_enviar);
+        add(barra);
+
+        texto_chat.setEditable(false);
+        setLayout(null);
         setTitle("Chat");
-        setSize(400,900);
+        setSize(400,700);
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         setVisible(true);
         }
